2284f8236082781255a04499a6ae0416fad55090
Kandy Peter <kandypeter03@gmail.com>
2022-02-28 17:16:47 +0200

    implement add function and redux store

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const BookForm = () => (
-  <div className="form-container">
-    <form>
-      <input type="text" placeholder="Book title..." />
-      <select name="category">
-        <option>Action</option>
-        <option>Drama</option>
-        <option>Science Fiction</option>
-      </select>
-      <input type="submit" value="ADD BOOK" />
-    </form>
-  </div>
-);
+const BookForm = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: new Date(),
+      title,
+      author,
+      category,
+    };
+    dispatch(addBook(newBook));
+    setTitle('');
+    setCategory('');
+    setAuthor('');
+  };
+  return (
+    <div className="form-container">
+      <form onSubmit={submitBookToStore}>
+        <input
+          type="text"
+          placeholder="Author name"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Book title..."
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <select
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option>Action</option>
+          <option>Drama</option>
+          <option>Science Fiction</option>
+        </select>
+        <input type="submit" value="ADD BOOK" />
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
